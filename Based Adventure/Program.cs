54720c49aef2aaafc192a949404b7d2c5de46cb3
Olle Astré <olle.astre@tutamail.com>
2022-09-09 12:04:10 +0200

    Implement combat mechanics & boss fight

--- Based Adventure/Program.cs
+++ Based Adventure/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Security;
 
 namespace Based_Adventure
@@ -26,6 +27,15 @@ namespace Based_Adventure
                         break;
                     case "lockedroom":
                         LockedRoom(hero);
+                        break;
+                    case "thirdroom":
+                        ThirdRoom(hero);
+                        break;
+                    case "outsideroom":
+                        OutsideRoom(hero);
+                        break;
+                    case "bossfight":
+                        BossFight(hero);
                         break;
                     default:
                         Console.Error.WriteLine($"You forgot to implement '{hero.Location}'!");
@@ -74,7 +84,7 @@ namespace Based_Adventure
             return new Random().Next() % 6 + 1;
             //         random number % 0-5 + 1
         }
-        
+
         // -------------------------------- Rooms -----------------------------------------------
 
         static void NewGame(Hero hero)
@@ -169,7 +179,7 @@ namespace Based_Adventure
         static void LockedRoom(Hero hero)
         {
             Console.Clear();
-            Console.WriteLine("Inside the locked room you find a shiny sword");
+            Console.WriteLine("You use the key on a locked door and walk in. \n" + "Inside the locked room you find a shiny sword");
 
             if (AskYesOrNo("Do you want it instead of your wooden sword? Yes/No: "))
             {
@@ -178,10 +188,78 @@ namespace Based_Adventure
                 Console.WriteLine("You picked the shiny sword.");
             }
             
+            Console.WriteLine("You proceed into the next room.");
             Console.WriteLine("Press any key to continue.");
-            Console.Read();
+            Console.ReadKey();
 
             hero.Location = "thirdroom";
+        }
+
+        static void OutsideRoom(Hero hero)
+        {
+            Console.Clear();
+            Console.WriteLine("As you enter the room a large minotaur wakes up and takes out his weapon. " +
+                              "Prepare for battle!");
+
+            string weaponChoice;
+            if (!hero.Items.Contains("Knife")) // player has no (kn/l)ife
+            {
+                if (hero.Items.Contains("Wooden Sword"))
+                {
+                    Console.WriteLine("You take out your wooden sword.");
+                    hero.EquipedWeapon = "Wooden Sword";
+                }
+                else // only shiny sword
+                {
+                    Console.WriteLine("You take out your shiny sword. It glows a little.");
+                    hero.EquipedWeapon = "Shiny Sword";
+                }
+            }
+            else // player has a knife. If they have a knife, they cannot access the Shiny Sword
+            {
+                weaponChoice = Ask("Which weapon do you wish to use? Knife/Wooden Sword: ").ToLower();
+                switch (weaponChoice)
+                {
+                    case "wooden sword":
+                    case "sword":
+                        Console.WriteLine("You take out your wooden sword.");
+                        hero.EquipedWeapon = "Wooden Sword";
+                        break;
+                    case "knife":
+                        Console.WriteLine("You take out your Knife.");
+                        hero.EquipedWeapon = "Knife";
+                        break;
+                }
+            }
+            
+            Console.WriteLine("Press any key to start battle.");
+            Console.ReadKey();
+
+            hero.Location = "bossfight";
+        }
+
+        static void BossFight(Hero hero, Enemy enemy)
+        {
+            Console.Clear();
+            while (!enemy.IsDead)
+            {
+                int damage = hero.HeroAttack();
+                enemy.Health -= damage;
+                damage = enemy.RollEnemyDamage(hero);
+                
+                string playerChoice = Ask("The minotaur is gearing up for an attack: Dodge/Jump/Parry ").ToLower();
+                switch (playerChoice)
+                {
+                    case "dodge":
+                        int roll = RollD6();
+                        if (roll <= 1)
+                        {
+                            damage = 0;
+                            Console.WriteLine($"You dodge the {enemy.Name}'s attack");
+                        }
+                }
+                
+            }
         }
 
         static void ThirdRoom(Hero hero)
@@ -190,7 +268,7 @@ namespace Based_Adventure
             Console.WriteLine("On the floor before you lies a lifeless corpse.\n" +
                               "Its hand is clasped around something shiny.\n");
 
-            if (AskYesOrNo("Do you want to loot the corpse or leave it?"))
+            if (AskYesOrNo("Do you want to loot the corpse? Yes/No: "))
             {
                 Console.WriteLine("You pick up an old silver necklace.");
                 if (RollD6() >= 3)
@@ -205,9 +283,11 @@ namespace Based_Adventure
                 }
             }
             
-            Console.WriteLine("You leave corpse and continue into the next room. \n" +
+            Console.WriteLine("You leave the corpse and continue into the next room. \n" +
                               "Press any key to continue.");
-            Console.Read();
+            Console.ReadKey();
+            
+            hero.Location = "outsideroom";
         }
     }
     class Hero
@@ -216,5 +296,58 @@ namespace Based_Adventure
         public int Health = 100;
         public List<string> Items = new List<string>(); // Note: use uppercase first letter for items.
         public string Location = "newgame";
+        public string EquipedWeapon = "";
+
+        public int HeroAttack()
+        {
+            int damage = 0;
+            Console.WriteLine("Roll the dice to determine your attack damage\n" + "Press any key to continue");
+            Console.ReadKey();
+            if (EquipedWeapon == "Shiny Sword")
+            {
+                damage = new Random().Next() % 6 + 15; // 15-20 damage
+                Console.WriteLine("You sliced though the tough hide of the minotaur. \n" + 
+                                  $"Your attack dealt {damage} damage");
+                return damage;
+
+            }
+            else if (EquipedWeapon == "Wooden Sword")
+            {
+                Console.WriteLine("You gave the minotaur a splinter with your wooden sword. \n" + 
+                                  $"Your attack dealt {damage} damage");
+                damage = new Random().Next() % 7 + 10; // 10-16 damage
+                return damage;
+            }
+            else // Knife
+            {
+                damage = (new Random().Next() % 8 + 3); // 3-10 damage
+                int damage2 = (new Random().Next() % 8 + 3); // 3-10 damage
+                Console.WriteLine("You stabbed twice with your knife. \n" + 
+                                  $"Your attacks dealt {damage} and {damage2} damage");
+                damage += damage2;
+                return damage;
+            }
+        }
+    }
+    
+    class Enemy
+    {
+        public string Name = "";
+        public int Health = 100;
+        public bool IsDead = false;
+
+        public Enemy(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+        
+        public int RollEnemyDamage(Hero hero)
+        {
+            if (hero.Items.Contains("Cursed Amulet"))
+                return new Random().Next() % 6 + 10; // 10-15 damage
+
+            else return new Random().Next() % 6 + 5; // 5-10 damage
+        }
     }
 }
